68f30454ef802b56665711e8b4dd76ffd7493b6b
David Costa <davidamorimc@gmail.com>
2023-07-03 23:25:19 +0100

    concurrency

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,15 +4,12 @@ import { useState } from "react";
 import { Grid, Typography } from "@mui/material";
 
 function Body() {
-  const [something] = useState("Hello World");
+  const [something] = useState("Hello World!!");
 
   return (
     <Grid container direction={"column"} className="main-paper">
       <Grid item xs={12}>
         <Typography>{something}</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Typography>Happy Hacking!!</Typography>
       </Grid>
     </Grid>
   );
