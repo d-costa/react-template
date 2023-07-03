a9e0a196c5c437604a5bda3c0369d23e8a6d5a51
David Costa <davidamorimc@gmail.com>
2023-07-03 22:37:31 +0100

    preview

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,19 +1,19 @@
 // React component with two input values
 
 import { useState } from "react";
-import {
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 function Body() {
   const [something] = useState("Hello World");
 
   return (
-    <Grid container className="main-paper">
-      <Typography>
-        {something}
-      </Typography>
+    <Grid container direction={"column"} className="main-paper">
+      <Grid item xs={12}>
+        <Typography>{something}</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Happy Hacking!!</Typography>
+      </Grid>
     </Grid>
   );
 }
